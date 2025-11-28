698e5df752e3bc8f048207f9abb5cb632d364c2d
Abel Sangare <abelsenysangare1@gmail.com>
2025-11-28 16:24:36 +0000

    Ajout Procfile, requirements.txt, runtime.txt et settings production

--- BMTech/settings.py
+++ BMTech/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["BMTech.up.railway.app"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',

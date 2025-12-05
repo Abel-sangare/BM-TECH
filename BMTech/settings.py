53b9ba7dce0a2ce7ec02cfd002938797f671e3a7
Abel Sangare <abelsenysangare1@gmail.com>
2025-12-05 14:35:26 +0000

    Update ALLOWED_HOSTS and DEBUG for production

--- BMTech/settings.py
+++ BMTech/settings.py
@@ -5,9 +5,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'web-production-94b23.up.railway.app',
+    '.up.railway.app',
+    'bmtech.bm',
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
